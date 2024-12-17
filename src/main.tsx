b9bd33c9e349a2cc880baa80a01d50334b1fe518
Baroka-wp <birotori@gmail.com>
2024-12-17 08:52:55 +0100

    build: add google analyse

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import ReactGA from "react-ga4";
 import './index.css';
+
+
+ReactGA.initialize("G-3FTP4173P9");
+
+
+ReactGA.send({ 
+  hitType: "pageview", 
+  page: window.location.pathname
+});
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
